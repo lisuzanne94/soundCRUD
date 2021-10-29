ef52454c86eece5525b2c49915d988be4599df25
Suzi <lisuzanne94@gmail.com>
2021-10-29 17:20:58 -0400

    Added application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,32 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    return nil unless session[:session_token]
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def ensure_logged_in
+    render json: { message: "Hi" } unless logged_in?
+  end
+
+  def ensure_logged_out
+    render json: { message: "Bye" } if logged_in?
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def login!(user)
+    user.reset_session_token!
+    session[:session_token] = user.session_token
+    @current_user = user
+  end
+
+  def logout!
+    current_user.reset_session_token!
+    session[:session_token] = nil
+    @current_user = nil
+  end
 end
