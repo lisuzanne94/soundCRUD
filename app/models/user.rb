8f769b1a64229921f69af18ef9d7b3c58d92b3ff
Suzi <lisuzanne94@gmail.com>
2021-10-29 17:10:28 -0400

    Finished backend user auth

---
+++ app/models/user.rb
@@ -0,0 +1,39 @@
+class User < ApplicationRecord
+
+  after_initialize :ensure_session_token
+
+  validates :username, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: { message: "Password can't be blank" }
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    (user && user.is_password?(password)) ? user : nil
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(32)
+  end
+
+  attr_reader :password
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+    @password = password
+  end
+
+  def is_password?(password)
+    password_object = BCrypt::Password.new(self.password_digest)
+    password_object.is_password?(password)
+  end
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+    self.session_token
+  end
+end
