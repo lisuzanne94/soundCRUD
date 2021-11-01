bafb030bb7ef979133ceceb39aaacdb25af8d5e5
Suzi <lisuzanne94@gmail.com>
2021-11-01 09:22:55 -0400

    Added Track model

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,3 +1,8 @@
 class Track < ApplicationRecord
+    validates :title, :uploader_id, presence: true
 
+    belongs_to :uploader,
+        primary_key: :id,
+        foreign_key: :uploader_id,
+        class_name: :User
 end
