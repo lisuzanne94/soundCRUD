9f2be3f6f8c566050d438e7e5de7c153e088efaa
Suzi <lisuzanne94@gmail.com>
2021-11-01 15:42:36 -0400

    Added tracks reducer to entities reducer

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -1,13 +1,14 @@
 import { RECEIVE_CURRENT_USER } from "../actions/session_actions";
 
-const usersReducer = (state={}, action) => {
+const usersReducer = (state = {}, action) => {
     Object.freeze(state);
-    const nextState = Object.assign({}, state);
+    const nextState = Object.assign({}, state)
 
     switch (action.type) {
         case RECEIVE_CURRENT_USER:
-            nextState[action.currentUser.id] = action.currentUser
-            return nextState;
+            // nextState[action.currentUser.id] = action.currentUser
+            // return nextState;
+            return { [action.currentUser.id]: action.currentUser }
         default:
             return state;
     }
