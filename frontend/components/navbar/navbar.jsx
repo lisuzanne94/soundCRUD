18eb75f19ace4e2bb5e87e619f2bd09e92d0b17f
Suzi <lisuzanne94@gmail.com>
2021-11-04 16:08:00 -0400

    Added basic navbar styling

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 class NavBar extends React.Component {
     
@@ -8,11 +8,33 @@ class NavBar extends React.Component {
         const { currentUser, logout, openModal } = this.props;
 
         const navBar = currentUser ? (
-            <div className="navbar">
-                <Link to="/discover"><img className="discover-logo" src="/soundcrud-logo-transparent-2.png" /></Link>
-                <span>Welcome, {currentUser.username}!</span>
-                <Link to="/upload">Upload</Link>
-                <button onClick={logout} to="/">Logout</button>
+            <div className="navbar-bg">
+                <div className="navbar">
+                    <div className="left-side-bar">
+                        <Link to="/" className="nav-link">
+                            <div className="left-nav-btn">
+                                <img className="discover-logo" src="/soundcrud-logo-transparent-3.png" />
+                            </div>
+                        </Link>
+
+                        <NavLink className="nav-link" className="nav-home-link" activeClassName="nav-btn-active" to="/discover">
+                            <div className="left-nav-btn">Home</div>
+                        </NavLink>
+                    </div>
+
+                    <div className="right-side-bar">
+                        <NavLink className="nav-link" to="/upload">
+                            <div className="nav-upload-btn right-nav-btn" activeClassName="nav-btn-active">Upload</div>
+                        </NavLink>
+                        
+                        <Link className="nav-link" to="#">
+                            <div className="nav-user-btn right-nav-btn">{currentUser.username}</div>
+                        </Link>
+                        <Link className="nav-link" to="/">
+                            <div className="logout-btn right-nav-btn" onClick={logout}>Logout</div>
+                        </Link>
+                    </div>
+                </div>
             </div>
         ) : (
             null
