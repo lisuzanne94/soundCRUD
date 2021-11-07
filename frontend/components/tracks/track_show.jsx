d6e002dcdf7a767d451b61699307b4cb42d69227
Suzi <lisuzanne94@gmail.com>
2021-11-07 14:43:27 -0500

    Added basic formatting and styling to track show page

--- frontend/components/tracks/track_show.jsx
+++ frontend/components/tracks/track_show.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlayCircle } from '@fortawesome/free-solid-svg-icons'
 import { Link } from "react-router-dom";
 import CommentFormContainer from "../comments/comment_form_container";
 import CommentItemContainer from "../comments/comment_item_container";
@@ -21,33 +23,40 @@ class TrackShow extends React.Component {
 
         if (!this.props.track) { return null }
 
-        // guests (always id: 1) will have the ability to edit/delete all tracks for now??
-        const editButton = this.props.currentUserId === (1 || this.props.track.uploader_id)? (
+        const editButton = this.props.currentUserId === this.props.track.uploader_id ? (
             <button onClick={() => this.props.openModal('Update Track')}>Update Track</button>
         ) : null
 
-        const deleteButton = this.props.currentUserId === (1 || this.props.track.uploader_id) ? (
+        const deleteButton = this.props.currentUserId === this.props.track.uploader_id ? (
             <button onClick={() => this.props.deleteTrack(this.props.track.id).then(() => this.props.history.push('/discover'))}>Delete Track</button>
         ) : null
 
         return (
-            <div className="track-show-container">
+            <div className="track-show-page-container">
                 <div>
-                    <img className="show-track-cover-img" src={this.props.track.coverImage} />
+                    <div className="track-banner">
+                        <div className="track-banner-left">
+                            <div className="track-play-btn"><FontAwesomeIcon icon={faPlayCircle} /></div>
+                            <div className="track-banner-labels">
+                                <h2 className="track-banner-title">{this.props.track.title}</h2>
+                                <h3 className="track-banner-uploader">{this.props.track.uploader}</h3>
+                            </div>
+                        </div>
+                        
+                        <img className="track-show-cover-img" src={this.props.track.coverImage} />
+                    </div>
 
-                    <h2>{this.props.track.title}</h2>
-
-                    <p>Uploaded by: {this.props.track.uploader}</p>
-
-                    <Modal modalTrackId={this.props.trackId} />
-
-                    {editButton}
-                    {deleteButton}
-
-                    <div>
+                    <div className="comment-form">
                         <CommentFormContainer 
                             commentTrackId={this.props.trackId}
                         />
+                    </div>
+
+                    <Modal modalTrackId={this.props.trackId} />
+
+                    <div className="modify-track-btns">
+                        {editButton}
+                        {deleteButton}
                     </div>
 
                     <div>
