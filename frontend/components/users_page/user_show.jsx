07a4b059bcae90cdc2b7893ccf2141d2a18aee9d
Suzi <lisuzanne94@gmail.com>
2021-11-08 12:33:11 -0500

    Added ability to change user profile pic

--- frontend/components/users_page/user_show.jsx
+++ frontend/components/users_page/user_show.jsx
@@ -2,8 +2,32 @@ import React from "react";
 
 class UserShow extends React.Component {
 
+    constructor (props) {
+        super(props);
+        this.state = {
+            ...this.props.user,
+            profilePic: null
+        }
+
+        this.handleFile = this.handleFile.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
     componentDidMount () {
         this.props.fetchUser(this.props.match.params.userId)
+    }
+
+    handleFile(e) {
+        this.setState({ profilePic: e.currentTarget.files[0] });
+    }
+
+    handleSubmit (e) {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append('user[id]', this.state.id);
+        formData.append('user[profile_pic]', this.state.profilePic);
+
+        this.props.updateUser(formData);
     }
 
     render () {
@@ -16,7 +40,15 @@ class UserShow extends React.Component {
         return (
             <div>
                 {user.username}
-                <img src={user.profilePic} />
+
+                <form onSubmit={this.handleSubmit}>
+                    <input type="file" 
+                        onChange={this.handleFile}
+                    />
+                    <button>Submit</button>
+                </form>
+
+                <img className="user-profile-pic" src={user.profilePic} />
                 <div>
                     <ul>
                         {
--- frontend/components/users_page/user_show_container.jsx
+++ frontend/components/users_page/user_show_container.jsx
@@ -1,13 +1,14 @@
 import { connect } from "react-redux";
 import UserShow from "./user_show";
-import { fetchUser } from "../../actions/user_actions";
+import { fetchUser, updateUser } from "../../actions/user_actions";
 
 const mSTP = (state, ownProps) => ({
     user: state.entities.users[ownProps.match.params.userId]
 });
 
 const mDTP = dispatch => ({
-    fetchUser: userId => dispatch(fetchUser(userId))
+    fetchUser: userId => dispatch(fetchUser(userId)),
+    updateUser: user => dispatch(updateUser(user))
 });
 
 export default connect(mSTP, mDTP)(UserShow);
