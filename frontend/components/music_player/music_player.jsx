c96087c052d4967b84525d894e563e12b1c2f9b2
Suzi <lisuzanne94@gmail.com>
2021-12-03 17:58:57 -0500

    refactor music player for hooks

--- frontend/components/music_player/music_player.jsx
+++ frontend/components/music_player/music_player.jsx
@@ -1,88 +1,151 @@
-import React from "react";
+// import React from "react";
+// import { Link } from "react-router-dom";
+
+// class MusicPlayer extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             duration: 0,
+//             currentTime: 0
+//         }
+
+//         this.handleSeek = this.handleSeek.bind(this);
+//     }
+
+//     componentDidMount () {
+//         this.props.receivePlayTrack(this.props.track)
+//     }
+
+//     componentWillUnmount () {
+//         this.props.clearPlayTrack();
+//     }
+
+//     handleSeek (e) {
+//         // const track = document.getElementById("current-song");
+//         this.setState({ currentTime: e.currentTarget.value })
+//         console.log(this.state.currentTime)
+//     }
+
+//     getDuration () {
+//         const track = document.getElementById("current-song");
+//         return track.duration
+//     }
+
+//     pauseTrack () {
+//         const track = document.getElementById("current-song");
+//         track.pause()
+//     }
+
+//     playTrack () {
+//         const track = document.getElementById("current-song");
+//         track.play()
+//     }
+
+//     render() {
+
+//         if (!this.props.track ) { return null }
+
+//         const { track } = this.props;
+
+//         // console.log(document.getElementById("current-song") ? document.getElementById("current-song") : null)
+
+//         return (
+            // <div className="music-player-bar-container">
+            //     <div className="current-track-details">
+            //         <h3>Now playing: <Link className="current-track-title" to={`/tracks/${track.id}`}>{track.title}</Link></h3>
+            //         <p>Uploaded by: <Link className="current-track-uploader" to={`/users/${track.uploader.id}`}>{track.uploader.username}</Link></p>
+            //     </div>
+
+            //     <div className="music-player-container">
+            //     <audio 
+            //         onLoadedMetadata={(e) => this.setState({ duration: e.target.duration})}
+            //         controls
+            //         autoPlay
+            //         controlsList="nodownload"
+            //         className="music-player" 
+            //         id="current-song" 
+            //         src={this.props.track.trackFile} >
+            //     </audio>
+
+            //     <button onClick={this.pauseTrack}>Pause</button>
+            //     <button onClick={this.playTrack}>Play</button>
+
+            //     <div>
+            //         //round duration up
+            //         <input type="range" 
+            //             min="0"
+            //             max={this.state.duration}
+            //             onChange={this.handleSeek}
+            //         />
+            //     </div>
+            // </div>
+
+            // </div>
+//         )
+//     }
+// };
+
+// export default MusicPlayer;
+
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-class MusicPlayer extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         duration: 0,
-    //         currentTime: 0
-    //     }
+const MusicPlayer = ({ track, receivePlayTrack, clearPlayTrack }) => {
+    const [state, setState] = useState({
+        duration: 0,
+        currentTime: 0
+    })
 
-    //     this.handleSeek = this.handleSeek.bind(this);
-    // }
+    const currentTrack = document.getElementById("current-song");
 
-    componentDidMount () {
-        this.props.receivePlayTrack(this.props.track)
+    useEffect(() => {
+        receivePlayTrack(track);
+        return clearPlayTrack;
+    }, [track]);
+
+    const handleSeek = e => {
+        setState(() => ({ currentTime: e.currentTarget.value }))
+        console.log(e.currentTarget.value)
     }
 
-    componentWillUnmount () {
-        this.props.clearPlayTrack();
-    }
+    const playTrack = () => currentTrack.play();
 
-    // handleSeek (e) {
-    //     // const track = document.getElementById("current-song");
-    //     this.setState({ currentTime: e.currentTarget.value })
-    //     console.log(this.state.currentTime)
-    // }
+    const pauseTrack = () => currentTrack.pause();
 
-    // getDuration () {
-    //     const track = document.getElementById("current-song");
-    //     return track.duration
-    // }
 
-    // pauseTrack () {
-    //     const track = document.getElementById("current-song");
-    //     track.pause()
-    // }
+    return track ? (
+        <div className="music-player-bar-container">
+            <div className="current-track-details">
+                <h3>Now playing: <Link className="current-track-title" to={`/tracks/${track.id}`}>{track.title}</Link></h3>
+                <p>Uploaded by: <Link className="current-track-uploader" to={`/users/${track.uploader.id}`}>{track.uploader.username}</Link></p>
+            </div>
 
-    // playTrack () {
-    //     const track = document.getElementById("current-song");
-    //     track.play()
-    // }
-
-    render() {
-
-        if (!this.props.track ) { return null }
-
-        const { track } = this.props;
-
-        // console.log(document.getElementById("current-song") ? document.getElementById("current-song") : null)
-
-        return (
-            <div className="music-player-bar-container">
-                <div className="current-track-details">
-                    <h3>Now playing: <Link className="current-track-title" to={`/tracks/${track.id}`}>{track.title}</Link></h3>
-                    <p>Uploaded by: <Link className="current-track-uploader" to={`/users/${track.uploader.id}`}>{track.uploader.username}</Link></p>
-                </div>
-
-                <div className="music-player-container">
-                <audio 
-                    onLoadedMetadata={(e) => this.setState({ duration: e.target.duration})}
+            <div className="music-player-container">
+                <audio
+                    onLoadedMetadata={(e) => setState({ duration: e.target.duration })}
                     controls
                     autoPlay
                     controlsList="nodownload"
-                    className="music-player" 
-                    id="current-song" 
-                    src={this.props.track.trackFile} >
+                    className="music-player"
+                    id="current-song"
+                    src={track.trackFile} >
                 </audio>
 
-                {/* <button onClick={this.pauseTrack}>Pause</button>
-                <button onClick={this.playTrack}>Play</button>
+                <button onClick={pauseTrack}>Pause</button>
+                <button onClick={playTrack}>Play</button>
 
                 <div>
                     //round duration up
-                    <input type="range" 
+                    <input type="range"
                         min="0"
-                        max={this.state.duration}
-                        onChange={this.handleSeek}
+                        max={state.duration}
+                        onChange={handleSeek}
                     />
-                </div> */}
+                </div>
             </div>
-            
-            </div>
-        )
-    }
-};
+
+        </div>
+    ) : null
+}
 
 export default MusicPlayer;
